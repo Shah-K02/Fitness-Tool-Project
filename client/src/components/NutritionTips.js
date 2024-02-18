748a9ff8cee76c79a540d7b6799746d581feff71
Shah-K02 <116072342+Shah-K02@users.noreply.github.com>
2024-02-18 17:44:32 +0000

    made changes to the user home page, added a new component for nutrition tips and added a new component for posts, also made changes to the app.js file to include the new components

---
+++ client/src/components/NutritionTips.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const NutritionTips = () => {
+  return (
+    <div class="news">
+      <div class="news-heading">
+        <h2>Nutrition Tips</h2>
+        <p>Nutrition tips for you.</p>
+      </div>
+      <div class="news-container">
+        <div class="news-item">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            expedita soluta? Earum non natus vero veniam ipsa aperiam tempore
+            unde, quam dolores voluptatum modi ea ducimus, quae itaque ratione
+            illum!
+          </p>
+        </div>
+        <div class="news-item">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+            necessitatibus repellat temporibus provident ad ea nesciunt rem?
+            Velit, eos nam accusantium assumenda ducimus asperiores,
+            reprehenderit odit doloremque sapiente impedit odio.
+          </p>
+        </div>
+        <div class="news-item">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea harum
+            sit eos! Impedit nam repellendus aperiam ut doloremque alias eos
+            aliquam id! Ipsam numquam commodi, laudantium magnam velit
+            perspiciatis doloribus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NutritionTips;
---
+++ client/src/components/Posts.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div className="news">
+      <div className="news-heading">
+        <h2>Posts</h2>
+        <p>
+          Here is the latest news from our company. We have been working hard to
+          bring you the best products and services.
+        </p>
+      </div>
+      <div className="news-container">
+        <div className="news-item">
+          <img src="img4.jpg" alt="Image 1" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            expedita soluta? Earum non natus vero veniam ipsa aperiam tempore
+            unde, quam dolores voluptatum modi ea ducimus, quae itaque ratione
+            illum!
+          </p>
+        </div>
+        <div className="news-item">
+          <img src="img4.jpg" alt="Image 2" />{" "}
+          <a href="https://www.freepik.com/free-vector/illustration-gallery-icon_2922280.htm#query=placeholder&position=0&from_view=keyword&track=sph&uuid=a218c410-d8f6-45ef-89aa-b36719c05cdb">
+            Image by rawpixel.com
+          </a>{" "}
+          on Freepik
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+            necessitatibus repellat temporibus provident ad ea nesciunt rem?
+            Velit, eos nam accusantium assumenda ducimus asperiores,
+            reprehenderit odit doloremque sapiente impedit odio.
+          </p>
+        </div>
+        <div className="news-item">
+          <img src="img4.jpg" alt="Image 3" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea harum
+            sit eos! Impedit nam repellendus aperiam ut doloremque alias eos
+            aliquam id! Ipsam numquam commodi, laudantium magnam velit
+            perspiciatis doloribus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
