1651d6810595ce989cd28b757a157473a0c8b1b8
Shah-K02 <116072342+Shah-K02@users.noreply.github.com>
2024-02-18 19:08:46 +0000

    removed auth0 from the project planning on using myql for authentication and user data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 import "./App.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// Replace with your own Auth0 application's values
-const domain = "dev-pn3crq3siyrys6rb.us.auth0.com";
-const clientId = "NMuxWvCJEYGoNuyc7ObBiY8IVhI3WVRl";
-
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>
 );
