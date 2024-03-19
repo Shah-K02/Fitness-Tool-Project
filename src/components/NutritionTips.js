d09220f8784003bbfe650d4f4cbbe5ce9d426fe0
Shah-K02 <116072342+Shah-K02@users.noreply.github.com>
2024-03-19 00:21:31 +0000

    changed the news image added headers and content to the news section and the added some nutrition tips

--- src/components/NutritionTips.js
+++ src/components/NutritionTips.js
@@ -4,32 +4,40 @@ const NutritionTips = () => {
   return (
     <div className="news">
       <div className="news-heading">
-        <h2>Nutrition Tips</h2>
-        <p>Nutrition tips for you.</p>
+        <h1>Today's Nutrition Tips</h1>
       </div>
       <div className="news-container">
         <div className="news-item">
+          <h2>Incorporate a Variety of Foods</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-            expedita soluta? Earum non natus vero veniam ipsa aperiam tempore
-            unde, quam dolores voluptatum modi ea ducimus, quae itaque ratione
-            illum!
+            Aim to include a diverse range of foods in your diet to ensure you
+            get a broad spectrum of nutrients. Different colored fruits and
+            vegetables provide various vitamins and minerals, while whole
+            grains, lean proteins, and healthy fats are essential for overall
+            health. This variety helps in covering the wide range of nutrients
+            your body needs.
           </p>
         </div>
         <div className="news-item">
+          <h2>Stay Hydrated</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-            necessitatibus repellat temporibus provident ad ea nesciunt rem?
-            Velit, eos nam accusantium assumenda ducimus asperiores,
-            reprehenderit odit doloremque sapiente impedit odio.
+            Water is crucial for your body's functions, including digestion,
+            absorption, circulation, creation of saliva, transportation of
+            nutrients, and maintenance of body temperature. Drinking enough
+            water can also help with weight management by keeping you full and
+            reducing the likelihood of mistaking thirst for hunger. Aim for
+            about 8 glasses a day.
           </p>
         </div>
         <div className="news-item">
+          <h2>Limit Added Sugars</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea harum
-            sit eos! Impedit nam repellendus aperiam ut doloremque alias eos
-            aliquam id! Ipsam numquam commodi, laudantium magnam velit
-            perspiciatis doloribus!
+            Foods high in added sugars and heavily processed foods can be high
+            in calories but low in nutrients, contributing to weight gain,
+            cardiovascular diseases, and other health issues. Try to minimize
+            the intake of sugary drinks, sweets, and processed snacks, focusing
+            instead on whole foods that are as close to their natural state as
+            possible
           </p>
         </div>
       </div>
