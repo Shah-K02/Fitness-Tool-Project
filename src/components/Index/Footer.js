0e4eebff2c41feb35b5221dc5ab2173bcbda019f
Shah-K02 <116072342+Shah-K02@users.noreply.github.com>
2024-04-15 20:02:21 +0100

    removed footer

--- src/components/Index/Footer.js
+++ src/components/Index/Footer.js
@@ -1,58 +1,9 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
-import logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-logo">
-          <img src={logo} alt="Your Logo" />
-        </div>
-
-        <div className="footer-links">
-          <ul>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Privacy Policy</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="footer-social">
-          <ul className="social-icons">
-            <li>
-              <a href="#">
-                <FaFacebook />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FaGithub />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FaLinkedin />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="footer-bottom">
-        <p>&copy; 2024 Personalised Fitness Assistant. All rights reserved.</p>
-      </div>
+      <p>&copy; 2024 Personalised Fitness Assistant. All rights reserved.</p>
     </footer>
   );
 };
